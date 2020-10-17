d56c445c471fe6271315bb5249eca676bb19c78b
Паучок <paukart12@gmail.com>
2020-10-17 23:14:38 +0300

    Updates

---
+++ precache-manifest.a06c521fca63f4b93640d51b8d8134ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96d6e715830619b58168e021bf0af82",
+    "url": "/hello-sosed/index.html"
+  },
+  {
+    "revision": "f80dcb169e9f158e0176",
+    "url": "/hello-sosed/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "91d882c6500a44bac8d3",
+    "url": "/hello-sosed/static/css/main.ab6ba8d6.chunk.css"
+  },
+  {
+    "revision": "f80dcb169e9f158e0176",
+    "url": "/hello-sosed/static/js/2.0db83af1.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/hello-sosed/static/js/2.0db83af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91d882c6500a44bac8d3",
+    "url": "/hello-sosed/static/js/main.f2dd0970.chunk.js"
+  },
+  {
+    "revision": "12c2f693776fccc6c04b",
+    "url": "/hello-sosed/static/js/runtime-main.afb583b7.js"
+  }
+]);
